d418e5b7324b085911a4795c6ad5c845bbe0466a
Chris Kraft <kriggity@gmail.com>
2026-02-14 22:10:07 -0500

    add: left and right keys functionality

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,47 +1,59 @@
 "use strict";
-
 (function () {
   // Define vars
-  const tabs = document.querySelectorAll(".tab");
+  const tab = document.querySelectorAll(".tab");
   const tabpanel = document.querySelectorAll(".tabpanel");
-
+  let tabfocus = 0;
+  const updateTabFocus = (x) => {
+    tabfocus = x;
+    resetTabPanels();
+    updateTab(x);
+    updateTabPanel(x);
+  };
+  const resetTabPanels = () => {
+    tab.forEach((e) => {
+      // Set all tab buttons' aria-selected:false
+      e.setAttribute("aria-selected", "false");
+      // Set all tab buttons' tabindex:-1
+      e.setAttribute("tabindex", "-1");
+    });
+    // Set all tabpanels' to hidden
+    tabpanel.forEach((panel) => {
+      panel.removeAttribute("visible");
+      panel.setAttribute("hidden", "");
+    });
+  };
+  const updateTab = (x) => {
+    // Set this tab button's aria-selected:true
+    tab[x].setAttribute("aria-selected", "true");
+    // Remove this tab button's tabindex
+    tab[x].removeAttribute("tabindex");
+  };
+  const updateTabPanel = (x) => {
+    tabpanel[x].removeAttribute("hidden");
+    tabpanel[x].setAttribute("visible", "");
+  };
   // For each tab button
-  tabs.forEach((e, i) => {
+  tab.forEach((e, i) => {
     // Click event
     e.onclick = () => {
-      tabs.forEach((f) => {
-        // Set all tab buttons' aria-selected:false
-        f.setAttribute("aria-selected", "false");
-
-        // Set all tab buttons' tabindex:-1
-        f.setAttribute("tabindex", "-1");
-      });
-      // Set this tab button's aria-selected:true
-      e.setAttribute("aria-selected", "true");
-      // Remove this tab button's tabindex
-      e.removeAttribute("tabindex");
-
-      // Set all tabpanels' to hidden
-      tabpanel.forEach((panel) => {
-        panel.removeAttribute("visible");
-        panel.setAttribute("hidden", "");
-      });
-      // Set the corresponding tabpanel visible
-      tabpanel[i].removeAttribute("hidden");
-      tabpanel[i].setAttribute("visible", "");
+      updateTabFocus(i);
     };
-    // Need to add keyboard functionality
   });
+  // Need to add keyboard functionality
   document.addEventListener("keydown", function () {
     switch (event.key) {
       case "ArrowLeft":
-        // console.log(event.key);
+        if (tabfocus > 0) {
+          updateTabFocus(tabfocus - 1);
+        }
         break;
       case "ArrowRight":
-        // console.log(event.key);
+        if (tabfocus < tab.length - 1) {
+          updateTabFocus(tabfocus + 1);
+        }
         break;
       default:
-        // console.log("default");
         break;
     }
   });
